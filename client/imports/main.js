c51cae01c5703fee2c5c9cb1ddfdfe9e0cee69bd
Leonardo Romeiro <leonardo.romeiro@procempa.com.br>
2016-08-01 09:20:02 +0000

    meteor update 1.4

--- client/imports/main.js
+++ client/imports/main.js
@@ -10,25 +10,28 @@ import './pages/contato/contato';
 export class App {
 	configureRouter(config, router) {
 		config.title = "Meteor Aurelia Seed";
-		config.map([{
-			route: ["", "exemplo1"],
-			name: "exemplo1",
-			moduleId: "./pages/paginaExemplo1/exemplo1",
-			nav: true,
-			title: "Exemplo 1"
-		}, {
-			route: "exemplo2",
-			name: "exemplo2",
-			moduleId: "./pages/paginaExemplo2/exemplo2",
-			nav: true,
-			title: "Exemplo 2"
-		}, {
-			route: "contato",
-			name: "contato",
-			moduleId: "./pages/contato/contato",
-			nav: true,
-			title: "Contatos"
-		}]);
+		config.map([
+			{ route: '', redirect: 'exemplo1' },
+			{
+				route: "exemplo1",
+				name: "exemplo1",
+				moduleId: "./pages/paginaExemplo1/exemplo1",
+				nav: true,
+				title: "Exemplo 1"
+			}, {
+				route: "exemplo2",
+				name: "exemplo2",
+				moduleId: "./pages/paginaExemplo2/exemplo2",
+				nav: true,
+				title: "Exemplo 2"
+			}, {
+				route: "contato",
+				name: "contato",
+				moduleId: "./pages/contato/contato",
+				nav: true,
+				title: "Contatos"
+			}
+		]);
 		this.router = router;
 	}
 }
